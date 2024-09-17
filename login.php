d1f0b7182f449114fa57b13072367cca0a4fe06b
Joel Mack Kai En <135927685+moeljack@users.noreply.github.com>
2024-09-17 15:11:13 +0800

    login page done

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Check</title>
+</head>
+<body>
+    <form action="process_login.php" method="post">
+        <label for="userID">Enter User ID:</label>
+
+        <input type="text" id="userID" name="userID" value="<?php echo isset($_GET['userID']) ? htmlspecialchars($_GET['userID']) : ''; ?>" />
+
+        <input type="submit" value="Check User">
+
+        <?php
+        if (isset($_GET['error']) && $_GET['error'] === 'true') {
+            echo '<p style="color: red;">User ID does not exist. Please re-enter UserID.</p>';
+        }
+        ?>
+    </form>
+</body>
+</html>
---
+++ process_login.php
@@ -0,0 +1,37 @@
+<?php
+require_once "model/common.php";
+
+$connMgr = new ConnectionManager();
+$pdo = $connMgr->getConnection(); 
+
+// Check if userID is set
+if (isset($_POST['userID'])) {
+    $userID = $_POST['userID'];
+
+    // Prepare and execute the SQL statement
+    $sql = 'select * from employee where Staff_ID = :userID';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':userID', $userID, PDO::PARAM_INT);
+
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+
+    $result = $stmt->fetchAll();
+
+    if (count($result)> 0) {
+        // User ID exists
+        $_SESSION['userID'] = $userID;
+        header("Location: home.php"); // Redirect to another page
+        exit();
+    } else {
+        // User ID does not exist
+        header("Location: login.php?userID=" . urlencode($userID) . "&error=true");
+        exit();
+    }
+} else {
+    header("Location: home.php");
+    exit();
+}
+
+?>
