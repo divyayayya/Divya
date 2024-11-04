db0e6588d9f57b3c82f82a4a157627fbc5970399
Joel Mack Kai En <135927685+moeljack@users.noreply.github.com>
2024-11-04 14:38:50 +0800

    test azure db

--- model/ConnectionManager.php
+++ model/ConnectionManager.php
@@ -9,7 +9,13 @@
         $password = 'username0!';
 
         $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
-        $pdo = new PDO($dsn, $username, $password);
+
+        $options = [
+            PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,       // Enable SSL with the CA certificate
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,  // Optional: disable server certificate verification
+        ];
+
+        $pdo = new PDO($dsn, $username, $password, $options);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
         return $pdo;
         }
