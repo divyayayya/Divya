a93a31d8b84032f2f13606c1feaee01c1045383c
Joel Mack Kai En <135927685+moeljack@users.noreply.github.com>
2024-11-04 14:40:38 +0800

    test azure db

--- model/ConnectionManager.php
+++ model/ConnectionManager.php
@@ -11,7 +11,6 @@
         $dsn = "mysql:host=$host;port=$port;dbname=$dbname";
 
         $options = [
-            PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,       // Enable SSL with the CA certificate
             PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,  // Optional: disable server certificate verification
         ];
 
